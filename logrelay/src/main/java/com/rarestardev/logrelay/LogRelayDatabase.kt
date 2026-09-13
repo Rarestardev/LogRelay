19392abdaaae189eb6364e2ef847a731cefff6c4
Rarestardev <Rarestar.dev@gmail.com>
2026-09-13 08:59:17 +0330

    create instance from database in LogRelayDatabase.kt

--- logrelay/src/main/java/com/rarestardev/logrelay/LogRelayDatabase.kt
+++ logrelay/src/main/java/com/rarestardev/logrelay/LogRelayDatabase.kt
@@ -3,22 +3,25 @@ package com.rarestardev.logrelay
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import kotlin.jvm.java
 
 @Database(entities = [LogEntity::class], version = 1, exportSchema = false)
-abstract class LogRelayDatabase {
+abstract class LogRelayDatabase : RoomDatabase() {
 
     companion object {
         private const val DB_NAME = "LOG_RELAY"
         @Volatile private var INSTANCE: LogRelayDatabase? = null
 
         fun getInstance(context: Context): LogRelayDatabase {
-            return INSTANCE ?: synchronized(this) {
+            return INSTANCE ?: synchronized(context) {
                 Room.databaseBuilder(
-                    context.applicationContext,
-                    LogRelayDatabase::class.java,
-                    DB_NAME
+                    context = context,
+                    name = DB_NAME,
+                    klass = LogRelayDatabase::class.java
                 )
+                    .fallbackToDestructiveMigration(true)
+                    .build()
             }
         }
     }
