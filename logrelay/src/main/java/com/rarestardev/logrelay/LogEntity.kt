28ff5cfc195eff7e15aab73d6dbb10f493c0da1c
Rarestardev <Rarestar.dev@gmail.com>
2026-09-13 08:59:38 +0330

    create log writer entity

--- logrelay/src/main/java/com/rarestardev/logrelay/LogEntity.kt
+++ logrelay/src/main/java/com/rarestardev/logrelay/LogEntity.kt
@@ -3,7 +3,7 @@ package com.rarestardev.logrelay
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity("log_relay_col")
+@Entity("log_relay_table")
 data class LogEntity(
     @PrimaryKey(autoGenerate = true) val logId: Long = 0,
     val tag: String,
